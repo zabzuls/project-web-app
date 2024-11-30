37e930ae745c737c1d8c04332146d557bd0f26c1
Zabzul Supratman <zabzuls@gmail.com>
2024-11-30 16:44:32 +0700

    [update] loggin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-"use client"
-import React, { useEffect } from "react";
-import { redirect } from "next/navigation";
-import { usePathname } from "next/navigation";
+import ProfilePage from '@/components/ProfilePage/ProfilePage'
+import React from 'react'
 
 export default function Home() {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (pathname == "/") {
-      redirect("/login");
-    }
-  }, [pathname]);
-
-  return <div></div>;
+  return (
+    <div>
+      <ProfilePage/>
+    </div>
+  )
 }
